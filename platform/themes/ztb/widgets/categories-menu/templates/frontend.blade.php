49bb0298aecea0ea3dbc7b89fe8514667c58e951
ar2r <roooob66666666666@gmail.com>
2023-07-08 18:41:26 +0300

    blade button

--- platform/themes/ztb/widgets/categories-menu/templates/frontend.blade.php
+++ platform/themes/ztb/widgets/categories-menu/templates/frontend.blade.php
@@ -1,7 +1,7 @@
 <div class="sidebar-widget widget_categories mb-50">
-{{--    <div class="widget-header position-relative mb-20">--}}
-{{--        <h5 class="widget-title mt-5">{{__('Categories')}}</h5>--}}
-{{--    </div>--}}
+    {{--    <div class="widget-header position-relative mb-20">--}}
+    {{--        <h5 class="widget-title mt-5">{{__('Categories')}}</h5>--}}
+    {{--    </div>--}}
     <div class="post-block-list post-module-1 post-module-5">
         <ul>
             @foreach(get_popular_categories(5) as $category)
@@ -12,4 +12,4 @@
         </ul>
     </div>
 </div>
-<button></button>Перейти в светлый режим
+<input type="checkbox" onchange="colorThemeHandler(this.checked)" ></input>Перейти в светлый режим
